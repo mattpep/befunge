857b478f60f6fb5b213f0c66d8b9122717477d53
Matt Peperell <matt@peperell.com>
2022-08-25 01:56:04 +0100

    Process command-line args

--- befunge.rb
+++ befunge.rb
@@ -1,3 +1,36 @@
+require 'getoptlong'
+$debug = false
+opts = GetoptLong.new(
+  [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+  [ '--debug', '-d', GetoptLong::NO_ARGUMENT ]
+)
+
+USAGE = <<-EOT
+Usage:
+  #{$0} [options] <befunge.b93>
+
+-h, --help
+    show help
+
+-d, --debug
+    show debug output on stderr during exection
+EOT
+
+opts.each do |opt,arg|
+  case opt
+  when '--help'
+    STDERR.puts USAGE
+    exit 1
+  when '--debug'
+    $debug = true
+  end
+end
+
+def debug(s)
+  puts s if $debug
+end
+
+
 if ARGV.length != 1
   STDERR.puts "#{$0} [options] <befunge.b93>"
   exit 1
@@ -29,21 +62,21 @@ while true
       string_mode = false
     else
       stack << source[y][x].ord
-      STDERR.puts "Added character. Stack is now #{stack}"
+      debug "Added character. Stack is now #{stack}"
     end
   else
-    STDERR.puts "Current location is #{x},#{y}"
+    debug "Current location is #{x},#{y}"
     case source[y][x]
     when '"'
       string_mode = true
     when '<','v','>','^'
       direction = DIRECTIONS.invert[source[y][x]]
     when ':'
-      STDERR.puts "  dup"
+      debug "  dup"
       top = stack.pop.to_i
       2.times { stack << top }
     when '|'
-      STDERR.puts "  vIF"
+      debug "  vIF"
       top = stack.pop
       if top.nil? || top.zero?
         direction = :down
@@ -51,29 +84,29 @@ while true
         direction = :up
       end
     when '?'
-      STDERR.puts "  randdir"
+      debug "  randdir"
       direction = DIRECTIONS.keys.sample
     when ' ',nil
       nil
     when '.'
-      STDERR.puts "  printInt"
+      debug "  printInt"
       i = stack.pop
       print i
     when '@'
-      STDERR.puts "  EXIT"
+      debug "  EXIT"
       exit
     when ','
-      STDERR.puts "  printChar"
+      debug "  printChar"
       print stack.pop.chr
     when '1'..'9'
       stack << source[y][x].to_i
     else
-      STDERR.puts "row is #{source[y].join}"
+      debug "row is #{source[y].join}"
       raise BefungeError.new "Unimplemented operator: #{source[y][x].inspect}"
     end
   end
 
-  STDERR.puts "    x:#{x}, y:#{y}: current character is #{source[y][x]} and direction is #{direction}, stack is #{stack} (string repr: #{stack.map{ |c| (0...256).include?(c) ? c.chr : 'X' }.join})"
+  debug "    x:#{x}, y:#{y}: current character is #{source[y][x]} and direction is #{direction}, stack is #{stack} (string repr: #{stack.map{ |c| (0...256).include?(c) ? c.chr : 'X' }.join})"
 
   # move
   case direction
