fd25bee7c7c132c195a64972b8ff9e58e507e1d4
Matt Peperell <matt@peperell.com>
2022-08-25 01:50:25 +0100

    Add :|,@ operations and handle strings

--- befunge.rb
+++ befunge.rb
@@ -9,6 +9,7 @@ source = source_file.read.split("\n").map(&:chars)
 class BefungeError < StandardError; end
 
 stack = Array.new
+string_mode = false
 
 DIRECTIONS = {
   left: '<',
@@ -23,24 +24,53 @@ direction = :right
 
 
 while true
-  STDERR.puts "Current location is #{x},#{y}"
-  case source[y][x]
-  when '<','v','>','^'
-    direction = DIRECTIONS.invert[source[y][x]]
-  when '?'
-    STDERR.puts "  randdir"
-    direction = DIRECTIONS.keys.sample
-  when ' ',nil
-    nil
-  when '.'
-    STDERR.puts "  printInt"
-    i = stack.pop
-    print i
-  when '1'..'9'
-    stack << source[y][x].to_i
+  if string_mode
+    if source[y][x] == '"'
+      string_mode = false
+    else
+      stack << source[y][x].ord
+      STDERR.puts "Added character. Stack is now #{stack}"
+    end
   else
-    STDERR.puts "row is #{source[y].join}"
-    raise BefungeError.new "Unimplemented operator: #{source[y][x].inspect}"
+    STDERR.puts "Current location is #{x},#{y}"
+    case source[y][x]
+    when '"'
+      string_mode = true
+    when '<','v','>','^'
+      direction = DIRECTIONS.invert[source[y][x]]
+    when ':'
+      STDERR.puts "  dup"
+      top = stack.pop.to_i
+      2.times { stack << top }
+    when '|'
+      STDERR.puts "  vIF"
+      top = stack.pop
+      if top.nil? || top.zero?
+        direction = :down
+      else
+        direction = :up
+      end
+    when '?'
+      STDERR.puts "  randdir"
+      direction = DIRECTIONS.keys.sample
+    when ' ',nil
+      nil
+    when '.'
+      STDERR.puts "  printInt"
+      i = stack.pop
+      print i
+    when '@'
+      STDERR.puts "  EXIT"
+      exit
+    when ','
+      STDERR.puts "  printChar"
+      print stack.pop.chr
+    when '1'..'9'
+      stack << source[y][x].to_i
+    else
+      STDERR.puts "row is #{source[y].join}"
+      raise BefungeError.new "Unimplemented operator: #{source[y][x].inspect}"
+    end
   end
 
   STDERR.puts "    x:#{x}, y:#{y}: current character is #{source[y][x]} and direction is #{direction}, stack is #{stack} (string repr: #{stack.map{ |c| (0...256).include?(c) ? c.chr : 'X' }.join})"
